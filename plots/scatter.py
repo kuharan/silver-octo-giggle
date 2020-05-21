203520fc6fd65b59a4d66a47bddf04d4e29d2de7
kuharan <kuharan.bhowmik@gmail.com>
2020-05-21 22:01:00 +0530

    add scatter plot with layout and fig object

---
+++ plots/scatter.py
@@ -0,0 +1,26 @@
+import numpy as np
+import plotly.offline as pyo
+import plotly.graph_objs as go
+
+np.random.seed(41)
+
+random_x = np.random.randint(1,101,100)
+random_y = np.random.randint(1,101,100)
+
+data = [go.Scatter(x=random_x, 
+                    y=random_y, 
+                    mode='markers',
+                    marker=dict(
+                        size=15,
+                        color='rgb(51,200,150)',
+                        symbol='circle',
+                        line=dict(width=2)
+                    ))]
+
+layout = go.Layout(title='Scatter Plot',
+                    xaxis=dict(title='X Axis'),
+                    yaxis=dict(title='Y Axis'),
+                    hovermode='closest')
+
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='Scatter.html')
