03b016259ad126fee7e3db5ba3e71bb3211c5518
kuharan <kuharan.bhowmik@gmail.com>
2020-05-25 14:38:50 +0530

    modify bubble into two bubble examples

--- plots/bubble.py
+++ plots/bubble.py
@@ -5,7 +5,7 @@ import pandas as pd
 df = pd.read_csv(r'data\mpg.csv')
 print(df.head())
 
-data = [go.Scatter(x=df['horsepower'],
+horsepower_mpg = [go.Scatter(x=df['horsepower'],
                     y=df['mpg'],
                     text=df['name'],
                     mode='markers',
@@ -19,10 +19,31 @@ data = [go.Scatter(x=df['horsepower'],
                 )
         ]
 
-layout = go.Layout(title='Bubble Chart',
+displacement_acceleration = [go.Scatter(x=df['displacement'],
+                    y=df['acceleration'],
+                    text=df['name'],
+                    mode='markers',
+                    marker=dict(
+                        size=2*df['weight']/300,
+                        opacity=0.5,
+                        showscale=True
+                    )                    
+                )
+        ]
+
+layout1 = go.Layout(title='Bubble Chart',
                     hovermode='closest',
                     xaxis=dict(title='Horsepower'),
                     yaxis=dict(title='MPG')
                 )
-fig = go.Figure(data=data, layout=layout)
-pyo.plot(fig, filename=r'figure\bubble.html')
+
+layout2 = go.Layout(title='Bubble Chart',
+                    hovermode='closest',
+                    xaxis=dict(title='displacement'),
+                    yaxis=dict(title='acceleration')
+                )
+
+fig1 = go.Figure(data=horsepower_mpg, layout=layout1)
+fig2 = go.Figure(data=displacement_acceleration, layout=layout2)
+pyo.plot(fig1, filename=r'figure\bubble1.html')
+pyo.plot(fig2, filename=r'figure\bubble2.html')
